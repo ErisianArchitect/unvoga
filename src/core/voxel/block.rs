7ecf6cac1dae9ae2bd085125ce85a60f1623f45f
ErisianArchitect <derekforgione@gmail.com>
2024-07-15 11:49:22 -0700

    Updated Block

--- src/core/voxel/block.rs
+++ src/core/voxel/block.rs
@@ -1,13 +1,13 @@
 use std::any::Any;
 use crate::core::math::coordmap::Rotation;
 
-use super::{blocks::StateRef, blockstate::BlockState, coord::Coord, direction::Direction, engine::VoxelEngine, faces::Faces, lighting::lightargs::LightArgs, occluder::Occluder, occlusion_shape::OcclusionShape, tag::Tag, world::{occlusion::Occlusion, PlaceContext, VoxelWorld}};
+use super::{blocks::StateRef, blockstate::BlockState, coord::Coord, direction::Direction, engine::VoxelEngine, faces::Faces, lighting::lightargs::LightArgs, occluder::Occluder, occlusion_shape::OcclusionShape, prelude::Rgb, tag::Tag, world::{occlusion::Occlusion, PlaceContext, VoxelWorld}};
 
 pub trait Block: Any {
     fn name(&self) -> &str;
     fn as_any(&self) -> &dyn Any;
     fn as_any_mut(&mut self) -> &mut dyn Any;
-    fn occluder(&self, state: StateRef) -> &Occluder {
+    fn occluder(&self, world: &VoxelWorld, state: StateRef) -> &Occluder {
         const FULL_FACES: Occluder = Occluder::new(
             OcclusionShape::Full,
             OcclusionShape::Full,
@@ -18,23 +18,30 @@ pub trait Block: Any {
         );
         &FULL_FACES
     }
-    fn rotation(&self, coord: Coord, state: StateRef) -> Rotation {
+    fn material(&self, world: &VoxelWorld, coord: Coord, state: StateRef, face: Direction) -> () {
+        todo!()
+    }
+    fn color(&self, world: &VoxelWorld, coord: Coord, state: StateRef, face: Direction) -> Rgb {
+        Rgb::new(255, 0, 255)
+    }
+    fn rotation(&self, world: &VoxelWorld, coord: Coord, state: StateRef) -> Rotation {
         Rotation::new(Direction::PosY, 0)
     }
-    fn default_enabled(&self, coord: Coord, state: StateRef) -> bool { false }
-    fn light_args(&self, coord: Coord, state: StateRef) -> LightArgs {
+    fn default_enabled(&self, world: &VoxelWorld, coord: Coord, state: StateRef) -> bool { false }
+    fn light_args(&self, world: &VoxelWorld, coord: Coord, state: StateRef) -> LightArgs {
         LightArgs::new(15, 0)
     }
     fn neighbor_updated(&self, world: &mut VoxelWorld, direction: Direction, coord: Coord, neighbor_coord: Coord, state: StateRef, neighbor_state: StateRef) {}
     fn light_updated(&self, world: &mut VoxelWorld, coord: Coord, old_level: u8, new_level: u8) {}
     fn call(&self, world: &mut VoxelWorld, coord: Coord, state: StateRef, function: &str, arg: Tag) -> Tag { Tag::Null }
+    fn on_entity_collide(&self, world: &mut VoxelWorld, coord: Coord, state: StateRef, face: Direction, entity: ()) {}
     fn on_interact(&self, world: &mut VoxelWorld, coord: Coord, state: StateRef) {}
     fn on_update(&self, world: &mut VoxelWorld, coord: Coord, state: StateRef) {}
     fn on_place(&self, world: &mut VoxelWorld, context: &mut PlaceContext) { }
     fn on_remove(&self, world: &mut VoxelWorld, coord: Coord, old: StateRef, new: StateRef) {}
     fn on_data_set(&self, world: &mut VoxelWorld, coord: Coord, state: StateRef, data: &mut Tag) {}
     fn on_data_delete(&self, world: &mut VoxelWorld, coord: Coord, state: StateRef, data: Tag) {}
-    fn push_mesh(&self, mesh_builder: &mut (), coord: Coord, state: StateRef, occlusion: Occlusion) {}
+    fn push_mesh(&self, mesh_builder: &mut (), coord: Coord, state: StateRef, occlusion: Occlusion, rotation: Rotation) {}
     fn rotate(&self, coord: Coord, state: StateRef, rotation: Rotation) -> StateRef { state }
     fn default_state(&self) -> BlockState;
 
